c68fa94cf4a5d61f90966eba3da9f6c4558209fb
DeV <dsdjoy4@gmail.com>
2021-03-10 10:29:09 +1100

    New approach sucks

--- secondO.cpp
+++ secondO.cpp
@@ -12,35 +12,36 @@ class p1 {
     int randCalls = 0;
     int totalAtOrigin = 0;
     int totalUnchanged = 0;
-    vector<int> nums, loader;
+
 
   public:
-    p1() {
-      for (int i = 0; i < this->size; i++) this->loader.push_back(i);
+    p1() { //initialize the loader vector with 0-9
+
     }
 
   void setVector() {
+    vector<int> nums, loader;
     srand(time(0));
     int randIndex;
     for (int i = 0; i < this->size; i++) {
-      randIndex = rand() % this->loader.size();
+      randIndex = rand() % loader.size(); // get random number within the loader vector
       this->randCalls++;
-      this->nums.push_back(this->loader[randIndex]);
-      this->loader.erase(this->loader.begin() + randIndex);
+      nums.push_back(loader[randIndex]);
+      loader.erase(loader.begin() + randIndex); // remove the number from loader vector
     }
   }
 
   void atOrigin() {
     for (int i = 0; i < this->size; i++) {
-      if (this->nums[i] == i) this->totalAtOrigin++;
+      if (this->nums[i] == i) this->totalAtOrigin++; // calculate total at origin for each list
     }
-    this->totalUnchanged += this->totalAtOrigin;
+    this->totalUnchanged += this->totalAtOrigin; // calculate total at origin overall
   }
 
-  string returnInfo() {
+  string returnInfo() { // form the string for each list
     string vectorData = " ";
     for (int i = 0; i < this->size; i++) {
-      vectorData += to_string(this->nums[i]);
+      vectorData += to_string(nums[i]);
       vectorData += " ";
     }
     vectorData += "unchanged: " + to_string(this->totalAtOrigin) + " ";
@@ -52,18 +53,29 @@ class p1 {
     return this->totalUnchanged;
   }
 
-  void showVector() {
-    for (int i = 0; i < 10; i++) cout << this->nums[i];
-  }
+  // void showVector() {
+  //   for (int i = 0; i < 10; i++) cout << this->nums[i];
+  // }
 
 };
 
 int main() {
 
-  p1 *myO = new p1();
-  myO->setVector();
-  myO->atOrigin();
-  cout << myO->returnInfo() << endl;
+  vector<string> allInfo;
+  for (int i = 0; i < 20; i++) {
+    p1 *myO = new p1();
+    myO->setVector();
+    myO->atOrigin();
+    allInfo.push_back(myO->returnInfo());
+  }
+
+  for (int i = 0; i < 20; i++) {
+    cout << allInfo[i] << endl;
+  }
+  // p1 *myO = new p1();
+  // myO->setVector();
+  // myO->atOrigin();
+  // cout << myO->returnInfo() << endl;
 
 
   return 0;
